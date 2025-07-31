5065992202d164fe1fb323c968a4e0cd639d2dbb
ri7tech <36097919+RI7TE@users.noreply.github.com>
2025-07-30 23:30:29 -0500

    Fixed the __init__ imports

--- __init__.py
+++ __init__.py
@@ -5,10 +5,37 @@ from pathlib import Path
 
 sys.path.append(str(Path(__file__).parent.parent))
 
-from ReverseMap.convert import Convertible
+from ReverseMap.convert import Convertible, convertible, show
 from ReverseMap.rdict import rdict, rmap
-from ReverseMap.reverse import ReverseMap
+from ReverseMap.reverse import (
+    ReverseMap,
+    ReverseMapItems,
+    ReverseMapKeys,
+    ReverseMapping,
+    ReverseMapValues,
+)
 
 
 ReverseDict = ReverseMap
 ConvertibleValue = Convertible
+ReverseDictItems = ReverseMapItems
+ReverseDictKeys = ReverseMapKeys
+ReverseDictValues = ReverseMapValues
+
+__all__ = [
+    "Convertible",
+    'ConvertibleValue',
+    'ReverseDict',
+    'ReverseDictItems',
+    'ReverseDictKeys',
+    'ReverseDictValues',
+    'ReverseMap',
+    "ReverseMapItems",
+    "ReverseMapKeys",
+    "ReverseMapValues",
+    'ReverseMapping',
+    'convertible',
+    'rdict',
+    'rmap',
+    'show',
+]
